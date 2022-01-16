2d280d78d29e8080840c441c4f377fd7a68ac506
Sri Lekha Kakani <srilekha.kakani@gmail.com>
2022-01-16 11:22:37 +0530

    Phase 1 project

---
+++ src/com/srilekha/LockedMeMain.java
@@ -0,0 +1,13 @@
+package com.srilekha;
+
+public class LockedMeMain {
+public static void main(String[] args) {
+		
+		// Create "main" folder if not present in current folder structure
+		FileOperations.createMainFolderIfNotPresent("main");
+		
+		menuOptions.printWelcomeScreen("Locker", "Srilekha");
+		
+		HandleOption.handleWelcomeScreenInput();
+	}
+}
